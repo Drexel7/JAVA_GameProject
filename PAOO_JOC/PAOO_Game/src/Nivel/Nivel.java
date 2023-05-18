0ddc664c7b8f584c7cc199007280d2a6cfc42523
Drexel7 <maftei.mihaita@yahoo.com>
2023-05-18 16:23:57 +0300

    Initial commit

---
+++ PAOO_JOC/PAOO_Game/src/Nivel/Nivel.java
@@ -0,0 +1,19 @@
+package Nivel;
+
+public class Nivel {
+
+	private int[][] lvlData;
+
+	public Nivel(int[][] lvlData) {
+		this.lvlData = lvlData;
+	}
+
+	public int getSpriteIndex(int x, int y) {
+		return lvlData[y][x];
+	}
+
+	public int[][] getLevelData() {
+		return lvlData;
+	}
+
+}
